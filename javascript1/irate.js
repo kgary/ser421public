0717ac395111705874bed3a70f41f546e8fcb973
kgary <kgary@asu.edu>
2017-09-06 23:05:52 -0700

    conversion of irate java code to javascript, thanks to James Quigley

---
+++ javascript1/irate.js
@@ -0,0 +1,29 @@
+class Irate {
+
+    constructor(cap, rt) {
+        this.cap = cap;
+        this.rt = rt;
+        this.DEFAULT_RATE = 0.1;
+    }
+
+    computeYield(years, rate) {
+        return this.cap * this.yieldRate(years, rate);
+    }
+
+    yieldRate(years, rate) {
+        if (years <= 1) {
+            return 1 + rate;
+        } else {
+            let rt = this.yieldRate(years - 1, rate);
+            var crate = rt + rt * rate;
+            return crate;
+        }
+    }
+
+}
+
+var ir = new Irate(10000, 0.1);
+console.log(ir.constructor.name);
+
+console.log("yield: " + ir.computeYield(4, 0.1));
+console.log("yeild rate: " + ir.yieldRate(4, 0.1));
