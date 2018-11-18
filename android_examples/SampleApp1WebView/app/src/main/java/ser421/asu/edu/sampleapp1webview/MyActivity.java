b94b54985ff978b2484b53c3d653bab7bcbcd9bc
kgary <kgary@asu.edu>
2018-11-18 14:41:04 -0700

    Updated examples for 2018

--- android_examples/SampleApp1WebView/app/src/main/java/ser421/asu/edu/sampleapp1webview/MyActivity.java
+++ android_examples/SampleApp1WebView/app/src/main/java/ser421/asu/edu/sampleapp1webview/MyActivity.java
@@ -3,6 +3,7 @@ package ser421.asu.edu.sampleapp1webview;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MyActivity extends Activity {
     /**
@@ -13,7 +14,7 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
         WebView browser = (WebView)findViewById(R.id.webView);
-
+        //browser.setWebViewClient(new WebViewClient());
         browser.setHorizontalScrollBarEnabled(true);
         browser.setVerticalScrollBarEnabled(true);
         browser.setWebContentsDebuggingEnabled(true);
