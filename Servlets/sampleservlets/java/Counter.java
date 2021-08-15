6c3ed3715940dabf76da0661a05711b3730f28ef
Kevin Gary <kgary@asu.edu>
2021-08-15 14:45:28 -0700

    Delete Servlets directory

--- Servlets/sampleservlets/java/Counter.java
+++
@@ -1,8 +0,0 @@
-class Counter {
-    int count = 0;
-
-    public int incCounter() {return ++count;}
-    public int getCounter() {return count;}
-
-    public String toString() {return Integer.toString(count);}
-}
