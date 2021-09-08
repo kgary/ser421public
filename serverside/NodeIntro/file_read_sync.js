5b27719d54b3abeaf96583824a586d09548eb07b
kgary <kgary@asu.edu>
2021-09-08 11:24:56 -0700

    Updated for Buffer

--- serverside/NodeIntro/file_read_sync.js
+++ serverside/NodeIntro/file_read_sync.js
@@ -1,14 +1,15 @@
 // Example from Brad Dayley
 // https://github.com/bwdbooks/nodejs-mongodb-angularjs-web-development
+// Updated for Buffer 2021
 
 var fs = require('fs');
 fd = fs.openSync('./data/veggie.txt', 'r');
 var veggies = "";
 do {
-  var buf = new Buffer(5);
+  let buf = Buffer.alloc(5);
   buf.fill();
   var bytes = fs.readSync(fd, buf, null, 5);
-  console.log("read %dbytes", bytes);
+  console.log("read %d bytes", bytes);
   veggies += buf.toString();
 } while (bytes > 0);
 fs.closeSync(fd);
