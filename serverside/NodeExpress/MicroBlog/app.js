8151927a783b54846e3c51f580ef56daff8d8bcb
Abhishek Deore <dr.abhi7deore@gmail.com>
2021-09-28 13:21:27 -0700

    POST and PUT to add/replace blogs

--- serverside/NodeExpress/MicroBlog/app.js
+++ serverside/NodeExpress/MicroBlog/app.js
@@ -1,11 +1,63 @@
 var express = require('express');
 var message = require('./message/message');
+
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
+
 var app = express();
+app.use(fileUpload());
 app.listen(1337);
 
 app.get('/', function (req, res){
-  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8'});
-  res.end('A simple micro blog website with no frills nor nonsense.');
+  var form = '<html><form method="POST" enctype="multipart/form-data">' +
+        '<label for="file_upload">Upload a blog (markdown only):</label> <br><br>' +
+        '<input type="file" id="file_upload" name="file_upload" accept=".md"></input><br><br>'+
+        '<button>Submit</button></html>' +
+        '</form>';
+  res.send('A simple micro blog website with no frills nor nonsense.' + form);
+});
+
+app.post('/', function(req, res) {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    let uploadFile = req.files.file_upload;
+    let fileName = req.files.file_upload.name;
+
+    var ahref = '<a href=/>Return</a><br>'
+    fs.stat('./blog/'+fileName, function(err, stat) {
+        if(err == null) {
+            console.log('Blog exists');
+            return res.status(409).send('Blog already exists, upload failed.');
+        }
+    });
+
+    uploadFile.mv('./blog/'+fileName, function(err) {
+        if(err)
+          return res.status(500).send(err);
+        
+        var html = "Blog uploaded successfully! " + ahref;
+        res.send(html);
+    })
+});
+
+app.put('/', function(req, res) {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    let uploadFile = req.files.file_upload;
+    let fileName = req.files.file_upload.name;
+
+    uploadFile.mv('./blog/'+fileName, function(err) {
+        if(err)
+          return res.status(500).send(err);
+        
+        var ahref = '<a href=/>Return</a><br>'
+        var html = "Blog uploaded and updated successfully! " + ahref;
+        res.send(html);
+    })
 });
 
 app.get('/blog/:file', function (req, res){
