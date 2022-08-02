1eddf7656bd2782b91df9294755cd090b83cb636
Kevin Gary <kgary@asu.edu>
2022-08-02 15:00:12 -0700

    Refactored html5

--- clientside/html5/printenv.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-my $rand = int(rand(100));
-print "Set-Cookie: age=" . $rand . "\n";
-print "Content-type: text/html\n\n";
-while (($key, $val) = each %ENV) {
-	print "$key = $val<br>\n";
-}
