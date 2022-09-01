b5b2fd740cda46e2bac4f2bad8aeb1c803edb317
Kevin Gary <kgary@asu.edu>
2022-09-01 16:41:26 -0700

    Renaming and adding first SFC stuff

---
+++ vue3/sfc/SFCPlaygroundHelloWorld.vue
@@ -0,0 +1,21 @@
+// This is the example used in the SFC Playground, you can copy-paste to that tool and see the result
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<template>
+  <p class="greeting">{{ msg }}</p>
+</template>
+
+<style>
+.greeting {
+  color: red;
+  font-weight: bold;
+}
+</style>
