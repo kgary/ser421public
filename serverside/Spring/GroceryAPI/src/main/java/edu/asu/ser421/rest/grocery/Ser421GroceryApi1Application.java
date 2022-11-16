87dc3b7b7b78bf8aa2eab189f0528e9cb8f46458
Kevin Gary <kgary@asu.edu>
2022-11-16 09:45:02 -0700

    Build a REST API in Spring

---
+++ serverside/Spring/GroceryAPI/src/main/java/edu/asu/ser421/rest/grocery/Ser421GroceryApi1Application.java
@@ -0,0 +1,13 @@
+package edu.asu.ser421.rest.grocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ser421GroceryApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ser421GroceryApi1Application.class, args);
+	}
+
+}
