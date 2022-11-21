d1f168804b9472d8a4dd362d66c7bf40df08a261
kgary <kgary@asu.edu>
2022-11-21 11:34:37 -0700

    New iterations of the Grocery API

---
+++ javascript-intro/typescript/person.js
@@ -0,0 +1,13 @@
+const p = {
+    name: "Robert",
+    age: 39
+}; //person.ts
+/*
+  Uncomment this one to see a typescript compilation error
+  Also note in IDEs like VSCode you will get error highlighting
+
+const p2: Person = {
+  name : "Robert",
+  age : "thirty-nine"
+}
+*/ 
