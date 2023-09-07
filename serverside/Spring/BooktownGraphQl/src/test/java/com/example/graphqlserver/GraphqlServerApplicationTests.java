a5ccfa4938c6926b96ab27406a55881987dba514
skuma251 <skuma251@asu.edu>
2023-09-07 09:38:42 -0700

    Initial code for GraphQL

---
+++ serverside/Spring/BooktownGraphQl/src/test/java/com/example/graphqlserver/GraphqlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphqlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
