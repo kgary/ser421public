79683329daf419d0c63b2fad208c35d08e385f00
skuma251 <skuma251@asu.edu>
2023-09-11 00:52:12 -0700

    Updated error handling for 401 and 404

---
+++ AjaxFetch/fetch-weather-vue-multicomponent/src/utils/Modal.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="modal-overlay" @click="closeModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button class="close-button" @click="closeModal">X</button>
+        </div>
+
+        <div class="modal-body">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+}
+
+.modal-dialog {
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+  max-width: 80%;
+  width: 400px;
+}
+
+.modal-content {
+  padding: 20px;
+}
+
+.close-button {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-size: 20px;
+}
+</style>
